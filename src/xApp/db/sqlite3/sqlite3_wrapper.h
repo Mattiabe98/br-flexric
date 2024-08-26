bbb8cc78bc7f807bfb0af34868acf4c084f28ec6
Ayman Fatich <ayman.fatich.r@gmail.com>
2024-08-26 14:23:39 +0200

    add functions to fill the kpm table with the corresponding data

--- src/xApp/db/sqlite3/sqlite3_wrapper.h
+++ src/xApp/db/sqlite3/sqlite3_wrapper.h
@@ -21,7 +21,8 @@
 
 
 #ifndef SQLITE3_WRAPPER_XAPP_H
-#define SQLITE3_WRAPPER_XAPP_H 
+#define SQLITE3_WRAPPER_XAPP_H
+#define MAX_SQL_LENGTH 512 
 
 #include "../../../sm/agent_if/read/sm_ag_if_rd.h"
 #include "../../../lib/e2ap/e2ap_global_node_id_wrapper.h" 
@@ -33,6 +34,17 @@ void init_db_sqlite3(sqlite3** db, char const* db_filename);
 void close_db_sqlite3(sqlite3* db);
 
 void write_db_sqlite3(sqlite3* db, global_e2_node_id_t const* id, sm_ag_if_rd_t const* rd);
+
+typedef struct {
+  const char* name;
+  const char* unit;
+} measure_info_t;
+
+typedef struct {
+  char id[128];
+  char group[64];
+  char ran_ue_id[128];
+} ue_id_info_t;
 
 #endif
 
